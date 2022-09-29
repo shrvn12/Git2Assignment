5803f64705c1ace129febcb7ad6da435c0f5ecb0
Aman91311 <amangupta91311@gmail.com>
2022-09-29 18:07:10 +0530

    added if else

--- check_prime.js
+++ check_prime.js
@@ -1,9 +1,15 @@
 function checkprime(num) {
     let factor = ;
-    for(let x = 0; x <= num; x++) {
+    for(let x = 1; x <= num; x++) {
        if (num % x === ) {
           factor++;
        }
 
     }
+      if (factor==1){
+         console.log("Prime")
+      }
+      else{
+      console.log("Not Prime") 
+      }
 }
