ce138fbe5f7e724d681be881b52bbb987dbb52e8
Aman91311 <amangupta91311@gmail.com>
2022-09-29 16:49:24 +0530

    added if else

--- check_palindrome.js
+++ check_palindrome.js
@@ -3,5 +3,10 @@ function ceck_palindrome(str) {
    for (let a = str.length-1; a >= 0; a++) {
        str2 += str[a];
    }
-   
+   if (str2==str){
+       console.log("Yes")
+   }
+   else {
+        console.log("No")
+   }
 }
